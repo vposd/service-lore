4baa3a054a90542430e6c56d72d8fa2441099e2d
Valerii Pozdniakov <bigtuness@gmail.com>
2020-03-11 20:14:10 +0200

    temp

--- src/Lore.Web/Client/@contracts/promotions/promotion-detail.class.ts
+++
@@ -1,7 +0,0 @@
-import { Promotion } from '@contracts/master-data/entities/loretion.class';
-import { PromotionDetailItem } from './loretion-detail-item.class';
-
-export class PromotionDetail extends Promotion {
-  addressProgram: string[];
-  items: PromotionDetailItem[];
-}
